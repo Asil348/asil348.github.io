bef0935dc40c3aaa01ecb961cfef4ed553a1742c
Asil <webdev348@gmail.com>
2021-08-04 02:12:06 +0300

    rename about to skills

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import "./styles/App.css";
 
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import About from "./components/About";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <About />
+      <Skills />
     </div>
   );
 }
