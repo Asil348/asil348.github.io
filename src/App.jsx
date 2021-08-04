56f32c2ddf209939ab2d899be6e1a4b2c8b70695
Asil <webdev348@gmail.com>
2021-08-04 18:18:26 +0300

    add animation framework

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "bootswatch/dist/zephyr/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import AOS from "aos";
 import "./styles/App.css";
+
 
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 
 function App() {
+
+  useEffect(() => {
+    AOS.init();
+  })
+
   return (
     <div className="App">
       <Navbar />
